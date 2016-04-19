1807755fb2471c433b286fe99d91d452346191cd
Yojhan Rodriguez <Yojhanr95@gmail.com>
2016-04-18 23:42:36 -0500

    Creación de ventana principal admnistrador

--- src/Presentacion/Login.java
+++ src/Presentacion/Login.java
@@ -16,7 +16,7 @@ public class Login extends javax.swing.JFrame {
      */
     public Login() {
         initComponents();
-        this.setLocationRelativeTo(null); //Ubica ventana en el medio
+        this.setLocationRelativeTo(null); //Ubica ventana en la mitad de la pantalla
     }
 
     /**
@@ -37,7 +37,7 @@ public class Login extends javax.swing.JFrame {
         jButton1 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        setTitle("Inicio sesión - Servidor STIP");
+        setTitle("Inicio sesión - Módulo Administrador");
         setResizable(false);
 
         jLabel3.setFont(new java.awt.Font("Tahoma", 1, 48)); // NOI18N
@@ -105,14 +105,13 @@ public class Login extends javax.swing.JFrame {
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addComponent(jLabel3)
-                .addGap(125, 125, 125))
+                .addGap(128, 128, 128))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(6, 6, 6)
                 .addComponent(jLabel3)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGap(17, 17, 17)
                 .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
@@ -121,7 +120,9 @@ public class Login extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        // TODO add your handling code here:
+        VentanaPrincipal vp = new VentanaPrincipal();
+        this.setVisible(false);
+        vp.setVisible(true);
     }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
